4e43bf5f4e83815a32504227be9229871280dc2f
Philipp Maier <git@pmaier.eu>
2019-07-12 21:37:08 +0200

    Added Get-ADDirectoryServerOperationMasterRole.ps1

---
+++ Get-ADDirectoryServerOperationMasterRole.ps1
@@ -0,0 +1,81 @@
+function Get-ADDirectoryServerOperationMasterRole {
+    <#
+    .SYNOPSIS
+    Used to query fsmo owners.
+    
+    .DESCRIPTION
+    Used to query one or more domains for their fsmo owners.
+    
+    .PARAMETER DomainName
+    Defines one or more domainnames to query for fsmo owners. Defaults to env:USERDNSDOMAIN.
+    
+    .EXAMPLE
+    Another example of how to use this cmdlet
+    
+    .INPUTS
+    [System.String]
+    
+    .OUTPUTS
+    [PSCustomObject]
+    
+    .NOTES
+    Author:			Philipp Maier
+    Github:			https://github.com/philmph
+    Repository:		https://github.com/philmph/PWSH_Common_Functions
+    
+    Creationdate:	-
+    Version:		1.0
+    #>
+    
+    [CmdletBinding()]
+    
+    param (
+        [Parameter(Mandatory=$false,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [ValidateScript({
+            # Check for domain name pattern
+            if ($_ -match '^[0-9a-z]+(\.[0-9a-z]+)+$') {
+                $true
+            } else {
+                throw "'$_' is not a valid domain name."
+            }
+        })]
+        [string[]]$DomainName = $env:USERDNSDOMAIN
+    ) # param
+    
+    begin {
+        Set-StrictMode -Version 2
+    } # begin
+    
+    process {
+        # $Domain can only be empty when not defined by user and $env:USERDNSDOMAIN doesn't exist.
+        # If defined ValidateScript would throw. Example Get-ADDirectoryServerOperationMasterRole -Domain ''
+        Write-Verbose -Message "Domain is: $Domain"
+        if ([string]::IsNullOrEmpty($Domain)) {
+            throw 'Not in a windows domain'
+        }
+
+        foreach ($DomainObj in $DomainName) {
+            try {
+                Write-Verbose -Message "Gathering information for $DomainObj"
+                $Forest = Get-ADForest -Server $DomainObj
+                $Domain = Get-ADDomain -Server $DomainObj
+
+                [PSCustomObject]@{
+                    DomainName = $DomainObj
+                    DomainNamingMaster = $Forest.DomainNamingMaster
+                    SchemaMaster = $Forest.SchemaMaster
+                    InfrastructureMaster = $Domain.InfrastructureMaster
+                    PDCEmulator = $Domain.PDCEmulator
+                    RIDMaster = $Domain.RIDMaster
+                }
+            } catch {
+                Write-Error -Exception Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException -Message "$DomainName doesn't exist or cannot be reached."
+            } # try/catch
+        } # foreach
+    } # process
+    
+    end {} # end
+} # function
