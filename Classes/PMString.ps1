4c1a48beaf3882a835cdee7d35da56e9a995a3b4
Philipp Maier <git@pmaier.at>
2020-08-13 10:50:04 +0200

    Add ToCyrillic to PMString class

--- Classes/PMString.ps1
+++ Classes/PMString.ps1
@@ -6,21 +6,28 @@ Author:				Philipp Maier
 Author Git:			https://github.com/philmph
 
 Notes & Usage:
-This class is used to implement the missing Trunctuate function on System.String in .NET.
-Call with [PMString]::Trunctuate($String, $Length)
+This class is used to implement static string functions which don't
+exist on [System.String] in .NET
 
-Link:				
+Call with [PMString]::Trunctuate($String, $Length) to trunctuate.
+Call with [PMString]::ToCyrillic($String) to convert a string to Cyrillic.
+
+Link:
+https://github.com/philmphdev/PWSH_Common_Functions
 #################################################################
 #>
 
 class PMString
 {
     static [string] Trunctuate ([string]$String, [int16]$Length) {
-
         if ($String.Length -le $Length) {
             $Length = $String.Length
         }
 
         return $String.Substring(0, $Length)
+    }
+
+    static [string] ToCyrillic ([string]$Value) {
+        return ([Text.Encoding]::ASCII).GetString(([Text.Encoding]::GetEncoding("Cyrillic")).GetBytes($Value))
     }
 }
